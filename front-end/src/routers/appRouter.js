00375e2a40754f9ab5d9ab5e242435a0ff587528
Viral <patel.viral@northeastern.edu>
2020-11-21 20:55:20 -0500

    Updated route for preferences form

--- front-end/src/routers/appRouter.js
+++ front-end/src/routers/appRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 import Home from "../components/home";
+import PreferencesForm from "../components/preferences-form/preferences-form";
 import FindMatch from "../containers/find-match";
 
 const AppRouter = () => {
@@ -11,6 +12,8 @@ const AppRouter = () => {
         <Route path="/" exact component={Home} />
 
         <Route path="/find-match" exact component={FindMatch} />
+
+        <Route path="/set-mentor-preferences" exact component={PreferencesForm} />
 
         <Route path="*" exact render={() => <Redirect to="/" />} />
       </Switch>
